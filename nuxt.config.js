83a09dd403c32e7460dc4d656213585989aede1a
Gonzalo <gonzalo.mottaa@gmail.com>
2021-11-03 08:47:03 -0300

    fix: :ambulance: modify nuxt.config... change directory builded

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,5 +68,9 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: ["vee-validate/dist/rules"]
+  },
+  buildDir: "_nuxt",
+  build: {
+    publicPath: "_nuxt/dist/"
   }
 };
